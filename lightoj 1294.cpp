43f2767506c6611c9cdfa28c60628d245329777e
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-01-23 18:16:22 +0600

    Create lightoj 1294.cpp

---
+++ lightoj 1294.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        long long a,b;
+        cin>>a>>b;
+         cout<<"Case "<<i<<": "<<(a/2)*b<<endl;
+    }
+}
