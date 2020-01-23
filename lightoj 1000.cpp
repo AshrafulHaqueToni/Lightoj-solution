7dd536ec110e2e0c1e32070bc74988dcebfb83b0
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-01-23 17:41:38 +0600

    Create lightoj 1000.cpp

---
+++ lightoj 1000.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<"Case "<<i+1<<": "<<a+b<<endl;
+    }
+    return 0;
+}
