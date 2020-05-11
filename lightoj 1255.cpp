9ad32fc1543eb6d26be8d80d320099e374dd08ee
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-05-12 04:24:56 +0600

    Update lightoj 1255.cpp

--- lightoj 1255.cpp
+++ lightoj 1255.cpp
@@ -2,44 +2,60 @@
 using namespace std;
 
 #define mx 1000005
-char str[mx],subst[mx];
-int b[mx];
 
-void preprocess()
+char ch1[mx],ch[mx];
+int len1,len2;
+
+vector<int> build_lps()
 {
-    int m=strlen(subst);
-    int i=0,j=-1;
-    b[i]=-1;
-    while(i<m)
+    vector<int>tem(len2);
+    int idx=0;
+    for(int i=1;i<len2;)
     {
-        while(j>0 && subst[i]!=subst[j])j=b[j];
-        i++;
-        j++;
-        b[i]=j;
+        if(ch1[i]==ch1[idx])
+        {
+            tem[i]=idx+1;
+            idx++;
+            i++;
+        }
+        else
+        {
+            if(idx!=0)idx=tem[idx-1];
+            else tem[i]=idx,i++;
+        }
+       // cout<<i<<" "<<idx<<endl;
     }
+    return tem;
 }
+
 int kmp()
 {
-    int n=strlen(str);
-    int m=strlen(subst);
-    int i=0,j=0,cnt=0;
-    preprocess();
-    while(i<n)
+    int cnt=0;
+    vector<int>lps=build_lps();
+    int i=0,j=0;
+    while(i<len1)
     {
-        while(j>0 && str[i]!=subst[j])j=b[j];
-        i++;
-        j++;
-        if(j==m)
+        if(ch[i]==ch1[j])i++,j++;
+        else
         {
-            cnt++;
-            j=b[j];
+            if(j!=0)j=lps[j-1];
+            else i++;
+        }
+        if(j==len2)
+        {
+            j=lps[j-1];
+            cnt++;///koy bar ace sei tar jonno
+
         }
     }
     return cnt;
 }
+
 void solve(int ii)
 {
-    scanf(" %s %s",str,subst);
+    scanf("%s%s",ch,ch1);
+    len1=strlen(ch);
+    len2=strlen(ch1);
     printf("Case %d: %d\n",ii,kmp());
 }
 
@@ -50,3 +66,4 @@ int main()
     for(int i=1;i<=t;i++)solve(i);
     return 0;
 }
+
