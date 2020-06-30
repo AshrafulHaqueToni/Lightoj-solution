1031993105bed48172e49ea15c4214abc9490388
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-06-30 18:57:18 +0600

    Update lightoj 1215.cpp

--- lightoj 1215.cpp
+++ lightoj 1215.cpp
@@ -1,119 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define mx 1000005
 #define ll long long
-#define mx 2000010
 
-vector<ll>v;
-bool a[mx];
+int ar[mx];
+char ch[mx];
 
-void sieve()
+
+int n,m,k,ii;
+
+
+void solve()
 {
-    for(int i=3; i*i<=mx; i++)
+    ll a,b,c;
+    scanf("%lld%lld%lld",&a,&b,&c);
+    ll one=(a*b)/__gcd(a,b);
+    if(c%one)
     {
-        if(!a[i])
-        {
-            for(int j=i*i; j<=mx; j+=i)
+        printf("Case %d: impossible\n",++ii);
+        return;
+    }
+    ll another=1e18;
+    /// lcm(one,x)=c
+    /// (one*x)/__gcd(one,x)=c
+    /// x=(c/one)*__gcd(one,x)
+    /// x will be divisors of one times (c/one)
+
+   // cout<<one<<endl;
+    ll val=c/one;
+    for(ll i=1;i*i<=one;i++)
+    {
+         //  cout<<i<<endl;
+         if(one%i==0)
+         {
+             ll tem=val*i;
+            ll val1=__gcd(one,tem);
+            if((tem*one)/val1==c)
             {
-                a[j]=true;
+                another=min(another,tem);
             }
-        }
+             tem=one/i;
+            tem*=val;
+              val1=__gcd(one,tem);
+            if((tem*one)/val1==c)
+            {
+                another=min(another,tem);
+            }
+         }
     }
-
-    v.push_back(2);
-
-    for(int i=3; i<=mx; i+=2)
-    {
-        if(!a[i])
-            v.push_back(i);
-    }
-
-    //for(auto it: v)cout<<it<<" ";
+    printf("Case %d: %lld\n",++ii,another);
 }
-
-
+/*
+1
+1000000 1000000 2000000
+*/
 int main()
 {
-    // freopen("input.txt","r",stdin);
-
-    //ios_base::sync_with_stdio(false);
-    //cin.tie(false);
-
-    sieve();
-
-    int t;
-    //scanf("%d",&t);
-    cin>>t;
-
-    for(int ii=1; ii<=t; ii++)
-    {
-        ll a,b,lcm,c=1;
-
-        //scanf("%lld%lld%lld",&a,&b,&lcm);
-        cin>>a>>b>>lcm;
-
-        //printf("Case %d: ",ii);
-        //printf("Case %d: ",ii);
-        cout<<"Case "<<ii<<": ";
-
-        if(lcm%a!=0||lcm%b!=0)
-        {
-            //printf("impossible\n");
-            //printf("impossible\n");
-            cout<<"impossible\n";
-            continue;
-        }
-
-
-        for(int i=0; v.size(); i++)
-        {
-            if(lcm<v[i])
-                break;
-
-            int cnt1=0,cnt2=0,cnt3=0;
-
-            if(lcm%v[i]==0)
-            {
-
-                while(1)
-                {
-                    if(lcm%v[i]==0)
-                    {
-                        cnt3++;
-                        lcm/=v[i];
-                    }
-                    else
-                        break;
-
-                    if(a%v[i]==0)
-                    {
-                        cnt1++;
-                        a/=v[i];
-                    }
-
-                    if(b%v[i]==0)
-                    {
-                        cnt2++;
-                        b/=v[i];
-                    }
-                }
-
-                //cout<<a<<" "<<b<<" "<<lcm<<" "<<cnt1<<" "<<cnt2<<" "<<cnt3<<endl;
-
-
-                if(max(cnt1,cnt2)<cnt3)
-                {
-                    for(int j=1; j<=cnt3; j++)
-                        c*=v[i];
-                }
-            }
-
-        }
-
-        //printf("%lld\n",c);
-        cout<<c<<endl;
-
-    }
-
-    return 0;
+   //freopen("in.txt","r",stdin);
+   //freopen("out.txt","w",stdout);
+   int t=1;
+   scanf("%d",&t);
+   while(t--)solve();
+   return 0;
 }
