a06181b4ea88cfdc4b3d99db25ffd02c5e6a5c3a
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-07-02 14:52:26 +0600

    Update lightoj 1141.cpp

--- lightoj 1141.cpp
+++ lightoj 1141.cpp
@@ -1,150 +1,93 @@
-//***Bismillahir Rahmanir Rahim***//
-
-//*********************************//
-//******Ashraful Haque Toni********//
-//********Dept. of CSE,JnU*********//
-//email:ashrafulhaquetoni@gmail.com//
-//*******contact:01640690531*******//
-//*********************************//
-
-
 #include<bits/stdc++.h>
-
 using namespace std;
 
-#define         ash ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define         pb push_back
-#define         mp make_pair
-#define         sc(n) scanf("%d",&n);
-#define         scl(n) scanf("%I64d",&n);
-#define         sc2(m,n) scanf("%d%d",&m,&n);
-#define         sc2l(m,n) scanf("%I64d%I64d",&m,&n);
-#define         pf printf
-#define         Big(x,y) max(x,y);
-#define         Small(x,y) min(x,y);
-#define         input(array,size) for(int i=0;i<size;i++)cin>>array[i];
-#define         newline pf("\n");
-#define         f(s,l,in) for(ll i=s;i<l;i+=in)
-#define         Max INT_MAX
-#define         Min INT_MIN
-#define         pi acos(-1.0)
-#define         Memset(a,val) memset(a,val,sizeof(a));
+#define mx 1005
+#define ll long long
 
-typedef long long ll;
+int ar[mx];
+char ch[mx];
+vector<int>fact[mx];
+int spf[mx],dist[mx];
+int n,m,k,ii;
 
-
-
-
-
-vector<int>v[1005];
-
-int visit[1005];
-
-int u,vv;
-
-void primefactorize(int n)
+void seive()
 {
-    int div=2,add=0,ind=n;
-    while(n>1)
+    for(int i=2;i<=mx-5;i++)spf[i]=i;
+    for(int i=2;i*i<=mx-5;i++)
     {
-        add=0;
-        while(n%div==0)
+        if(spf[i]==i)
         {
-            add=div;
-            n/=div;
-
+            for(int j=i*i;j<=mx-5;j+=i)
+            {
+                if(spf[j]==j)spf[j]=i;
+            }
         }
-        if(add)
-            v[ind].pb(add);
-        div++;
     }
-    if(ind==add)
-        v[ind].clear();
+    for(int i=2;i<=mx-5;i++)
+    {
+        int val=i;
+        while(val>1)
+        {
+            int sp=spf[val];
+            while(val%sp==0)val/=sp;
+            if(sp!=i)fact[i].push_back(sp);
+        }
+    }
+//    for(int i=2;i<=100;i++)
+//    {
+//        cout<<i<<" :";
+//        for(auto it:fact[i])cout<<it<<" ";
+//        cout<<endl;
+//    }
 }
 
-int bfs()
+void bfs(int st)
 {
-    Memset(visit,-1);
-
     queue<int>q;
-    q.push(u);
-    visit[u]=0;
-
+    q.push(st);
+    dist[st]=0;
     while(!q.empty())
     {
-        int x=q.front();
-        int to;
+        int u=q.front();
+       // cout<<u<<endl;
         q.pop();
-        for(int i=0; i<(int)v[x].size(); i++)
+        for(auto it:fact[u])
         {
-            to=x+v[x][i];
-            if(to<=vv&&visit[to]==-1)
-            {
-                visit[to]=visit[x]+1;
-                q.push(to);
-                if(to==vv)
-                    return visit[vv];
-
-            }
+            if(it+u>m)continue;
+            int val=it+u;
+            if(dist[val]!=-1 && dist[val]<=dist[u]+1)continue;
+            dist[val]=dist[u]+1;
+            q.push(val);
         }
     }
-    return visit[vv];
+}
 
-}/*
-
-int bfs( ){
-    int dist[1001];
-    memset(dist, -1, sizeof(dist));
-    dist[u] = 0;
-    queue < int > q;
-    q.push(u);
-    int from, to;
-    while( !q.empty() ){
-        from = q.front();
-        q.pop();
-        for( int i = 0; i < (int) v[from].size(); i++ ){
-            to = from + v[from][i];
-            if( to <= vv && dist[to] == -1 ) {
-                q.push(to);
-                dist[to] = dist[from] + 1;
-                if( to  == vv) return dist[vv];
-            }
-        }
+void solve()
+{
+    scanf("%d%d",&n,&m);
+    printf("Case %d: ",++ii);
+    if(n==m)
+    {
+        printf("0\n");
+        return;
     }
-    return dist[vv];
-}*/
-
+    if(n==1 || n>m)
+    {
+        printf("-1\n");
+        return;
+    }
+    memset(dist,-1,sizeof(dist));
+    bfs(n);
+    printf("%d\n",dist[m]);
+}
 
 int main()
 {
-    ash;
-    for(int i=2; i<=1005; i++)
-    {
-
-        primefactorize(i);
-
-    }
-    int t;
-    cin>>t;
-    for(int i=1; i<=t; i++)
-    {
-
-        cin>>u>>vv;
-
-        if(u==1)
-        {
-            cout<<"Case "<<i<<": "<<-1<<endl;
-            continue;
-        }
-        if(vv<=u)
-        {
-            cout<<"Case "<<i<<": "<<0<<endl;
-            continue;
-        }
-        cout<<"Case "<<i<<": "<<bfs()<<endl;
-
-
-    }
-
-    return 0;
+   //freopen("in.txt","r",stdin);
+   //freopen("out.txt","w",stdout);
+   int t=1;
+   seive();
+   scanf("%d",&t);
+   while(t--)solve();
+   return 0;
 }
