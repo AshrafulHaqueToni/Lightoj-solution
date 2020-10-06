fe03a85f47e8d1eb379e4c97060304f05e0aae9a
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-10-06 22:35:21 +0600

    Update lightoj 1005.cpp

--- lightoj 1005.cpp
+++ lightoj 1005.cpp
@@ -1,56 +1,45 @@
+// Created by ash_98
+      
 #include<bits/stdc++.h>
 using namespace std;
 
+#define mx 100005
 #define ll long long
+#define mod 1000000007
 
-ll dp[31][31];
-int vis[31][31],ii;
+int ar[mx];
+char ch[mx];
+int n,m,k,ii;
 
-ll func(int n,int r)
+void solve()
 {
-    if(r==1)return (ll)n;
-
-    if(n==r)return 1LL;
-
-    if(vis[n][r]==ii)return dp[n][r];
-
-    vis[n][r]=ii;
-
-    return dp[n][r]=func(n-1,r-1)+func(n-1,r);
+   scanf("%d%d",&n,&k);
+   if(k>n)printf("Case %d: 0\n",++ii);
+   else if(k==0)printf("Case %d: 1\n",++ii );
+   else
+   {
+       ll re=1;
+       int cnt=1;
+       ll ace=n-k+1;
+       for(int i=1;i<=k;i++)
+       {
+           re*=(ace*ace);
+           re/=cnt++;
+           ace++;
+       }
+       printf("Case %d: %lld\n",++ii,re );
+   }
+    
 }
 
 int main()
 {
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    int t;
-    scanf("%d",&t);
+  int t=1;
+  scanf("%d",&t);
+  while(t--)
+  {
+    solve();
+  }
 
-    for(ii=1;ii<=t;ii++)
-    {
-        int n,k;
-        scanf("%d%d",&n,&k);
-
-        printf("Case %d: ",ii);
-
-        if(n<k)
-        {
-            printf("0\n");
-            continue;
-        }
-        if(k==0)
-        {
-            printf("1\n");
-            continue;
-        }
-
-        ll re=1;
-
-        for(ll i=n-k+1;i<=n;i++)re*=i;
-        //cout<<re<<endl;
-
-        printf("%lld\n",func(n,k)*re);
-    }
-
-    return 0;
+  return 0;
 }
