5b39e789c9d9399d69122e694988f0c775bc462f
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-11-08 23:15:36 +0600

    Create lightoj 1311.cpp

---
+++ lightoj 1311.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long double
+ 
+void solve(int ii)
+{
+    ll a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+ 
+    ll t1=a/d;
+    ll t2=b/e;
+    cout<<"Case "<<ii<<": ";
+    cout<<setprecision(12)<<((a*t1+0.5*(-d)*t1*t1)+(b*t2+0.5*(-e)*t2*t2))<<" ";
+    cout<<setprecision(12)<<max(t1,t2)*c<<endl;
+ 
+}
+ 
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)solve(i);
+    return 0;
+}
