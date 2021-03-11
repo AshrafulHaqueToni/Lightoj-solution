159c17c9d782020a6ac652b6a7a3f150d6bf3154
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-03-12 01:16:04 +0600

    Create lightoj 1222.cpp

---
+++ lightoj 1222.cpp
@@ -0,0 +1,147 @@
+// Created by ash_98
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long T1;//for cost
+typedef long long T2;//for flow
+const int maxn = 200;
+const T1 INF = 1e12;
+const T2 inf = 1e12;
+const T1 eps = 0;
+struct Edge {
+    int from, to;
+    T2 cap, flow;
+    T1 cost;
+};
+
+int n,m,k,ii;
+
+struct MCMF {//0-indexed
+    int n, m, s, t;
+    vector<Edge> edges;
+    vector<int> G[maxn];
+    int p[maxn],inq[maxn];
+    T1 d[maxn];
+    T2 a[maxn];
+    void init() {
+        for(int i = 0; i < n; i++) G[i].clear();
+        edges.clear();
+    }
+    void AddEdge(int from,int to,T2 cap,T1 cost) {
+        edges.push_back((Edge){from, to, cap, 0, cost});
+        edges.push_back((Edge){to, from, 0, 0, -cost});
+        m = edges.size();
+        G[from].push_back(m-2);
+        G[to].push_back(m-1);
+    }
+    pair<T1,T2> Mincost() {//bellmanFord
+        T1 tot_cost = 0;
+        T2 tot_flow = 0;
+        while(true) {
+            for(int i = 0; i < n; i++) d[i] = INF;
+            d[s] = 0;
+            p[s] = 0;
+            a[s] = inf;
+            bool up=true;
+            while(up) {
+                up=false;
+                for(int u = 0; u < n; u++) {
+                    if(d[u]-INF>=-eps)continue;
+                    for(int j:G[u]) {
+                        Edge &e=edges[j];
+                        if(e.cap > e.flow && d[e.to] > d[u] + e.cost+eps) {
+                            d[e.to] = d[u] + e.cost;
+                            p[e.to] = j;
+                            a[e.to] = min(a[u], e.cap - e.flow);
+                            up=true;
+                        }
+                    }
+                }
+            }
+            if(abs(d[t]-INF)<=eps)break;
+            tot_cost += (T1)d[t] * a[t];
+            tot_flow += (T2)a[t];
+            int u = t;
+            while(u != s) {
+                edges[p[u]].flow += a[t];
+                edges[p[u]^1].flow -= a[t];
+                u = edges[p[u]].from;
+            }
+        }
+        return {tot_cost,tot_flow};
+    }
+    pair<T1,T2> Mincost2() {//SPFA
+        T1 tot_cost = 0;
+        T2 tot_flow = 0;
+        while(true) {
+            for(int i = 0; i < n; i++) d[i] = INF;
+            memset(inq, 0, sizeof(inq));
+            d[s] = 0;
+            inq[s] = 1;
+            p[s] = 0;
+            a[s] = inf;
+            queue<int> Q;
+            srand(time(NULL));
+            Q.push(s);
+            while(!Q.empty()) {
+                int u = Q.front();
+                Q.pop();
+                inq[u] = 0;
+                for(int i = 0; i < G[u].size(); i++) {
+                    Edge& e = edges[G[u][i]];
+                    if(e.cap > e.flow && d[e.to] > d[u] + e.cost+eps) {
+                        d[e.to] = d[u] + e.cost;
+                        p[e.to] = G[u][i];
+                        a[e.to] = min(a[u], e.cap - e.flow);
+                        if(!inq[e.to]) {
+                            Q.push(e.to);
+                            inq[e.to] = 1;
+                        }
+                    }
+                }
+            }
+            if(abs(d[t]-INF)<=eps)break;
+            tot_cost += (T1)d[t] * a[t];
+            tot_flow += a[t];
+            int u = t;
+            while(u != s) {
+                edges[p[u]].flow += a[t];
+                edges[p[u]^1].flow -= a[t];
+                u = edges[p[u]].from;
+            }
+        }
+
+        return {tot_cost,tot_flow};
+    }
+} mcmf;
+ 
+void solve()
+{
+	 scanf("%d",&n);
+	 mcmf.init();
+	 mcmf.n=n+n+2;
+	 mcmf.s=0;
+	 mcmf.t=n+n+1;
+	 for(int i=1;i<=n;i++)
+	 {
+	 	mcmf.AddEdge(0,i,1,0);
+	 	mcmf.AddEdge(i+n,n+n+1,1,0);
+	 	for(int j=1;j<=n;j++)
+	 	{
+	 		int x;
+	 		scanf("%d",&x);
+	 		mcmf.AddEdge(i,j+n,1,-x);
+            
+	 	}
+	 }
+	 printf("Case %d: %lld\n",++ii,-mcmf.Mincost2().first);
+}
+ 
+int main()
+{
+	int t=1;
+	scanf("%d",&t);
+	while(t--)solve();
+	return 0;
+}
