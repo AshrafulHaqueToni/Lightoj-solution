0e2b46cbe2c1a7c95b01687b34a3a74e39d92f00
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-03-16 14:53:53 +0600

    Update lightoj 1347.cpp

--- lightoj 1347.cpp
+++ lightoj 1347.cpp
@@ -1,7 +1,8 @@
+
 // Created by ash_98
 #include<bits/stdc++.h>
 using namespace std;
-#define mx 1000005
+#define mx 1000050
 
 int wa[mx],wb[mx],wv[mx],Ws[mx];
 //(1-indexed) sa[i] = starting position (0...n-1) of ith lexicographically smallest suffix in s
@@ -67,7 +68,7 @@ pair<int,int> Patterntern_occurence(string Text ,string Pattern)
     int m=Pattern.size();
     int be=1,en=n;
 
-    while(be<en) 
+    while(be<en)
     {
         int mid = (en+be)/2;
         int ok=0;
@@ -107,49 +108,127 @@ pair<int,int> Patterntern_occurence(string Text ,string Pattern)
 
     return re;
 }
+
+/// this is for LCP from index i to index j.
+/// just run a query from min(Rank[i-1],Rank[j-1])+1 to max(Rank[i-1],Rank[j-1])
+int ST[mx][22];
+int Jump_LOG[mx];
+
+void Build_Sparse(int n)
+{
+	for(int i=1;i<=n;i++)ST[i][0]=LCP[i];
+
+	for(int i=2;i<=n;i++)Jump_LOG[i]=Jump_LOG[i-1]+!(i&(i-1));
+
+	for(int j=1;(1<<j)<=n;j++)
+	{
+		for(int i=1;(i+(1<<j)-1)<=n;i++)
+		{
+           ST[i][j]=min(ST[i][j-1],ST[i+(1<<(j-1))][j-1]);
+		}
+	}
+}
+
+int query(int i,int j)
+{
+	int boro_lav=Jump_LOG[j-i+1];
+	return min(ST[i][boro_lav],ST[j-(1<<boro_lav)+1][boro_lav]);
+}
+
 int ii;
-int ID[mx];
+pair<int,int>ID[mx];
+int Baki[mx];
+bool nnw;
 
 void solve()
 {
-	string s,list[4];
-    int n=0;
-    int id=0;
-    for(int i=1;i<=3;i++)
-    {
-    	string t;
-    	cin>>t;
-    	int m=t.size();
-    	for(int j=0;j<m;j++)
-    	{
-    		ID[n++]=id;
 
-    	}
-    	s+=t;
-    	id++;
-    }
-   // cout<<s<<endl;
+	string s,ss;
+	int n=0;
+	int id=1;
+	int nibo=3;
+	for(int i=1;i<=nibo;i++)
+	{
+		cin>>ss;
+		int sz=ss.size();
+		for(int j=0;j<sz;j++)
+		{
+			ID[n]={id,n};
+			Baki[n++]=sz-j;
+		}
+		id++;
+		s+=ss;
+		if(i<nibo)s+="#",ID[n++]={-1,0};
+		
+	}
+
 	buildSA(s,sa,n+1,130); //Important
     buildLCP(s,sa,n);
-    int re=0;
+    Build_Sparse(n);
   
-    for(int i=3;i<=n;i++)
+    int re=0;
+    vector<int>pos;
+    int tem=0;
+    int be=nibo,en=be-1;
+    int cnt[nibo+5]={0};
+    int ace=0;
+    int need=nibo;
+
+    for(int i=nibo;i<=n;i++)
     {
+    	id=ID[sa[i]].first;
     	
-        if(ID[sa[i-1]]!=ID[sa[i-2]] && ID[sa[i]]!=ID[sa[i-1]] && ID[sa[i-2]]!=ID[sa[i]])
-        {
-        	re=max(re,min(LCP[i-1],LCP[i]));
-        }
+    	if(id==0)
+    	{
+    		en++;
+    		continue;
+    	}
+
+    	if(cnt[id]==0)ace++;
+    	cnt[id]++;
+    	en++;
+    	
+    	if(ace>=need)
+    	{
+			int koto=min(query(be+1,en),Baki[ID[sa[en]].second]);
+			re=max(re,koto);
+    	}
+
+    	while(be<=en && ace>=need)
+    	{
+    		id=ID[sa[be]].first;
+
+    		if(id==0)
+    		{
+    			be++;
+    			continue;
+    		}
+    		if(cnt[id]==1)ace--;
+    		be++;
+    		cnt[id]--;
+    		
+
+    		if(ace>=need)
+	    	{
+    			int koto=min(query(be+1,en),Baki[ID[sa[en]].second]);
+			    re=max(re,koto);
+	    	}
+
+    	}
 
     }
+
     printf("Case %d: %d\n",++ii,re );
+
+
 
 }
 
 int main()
 {
 	int t=1;
-	scanf("%d",&t);
-	while(t--)solve();
+    scanf("%d",&t);
+	while(t--)
+		solve();
 	return 0;
 }
