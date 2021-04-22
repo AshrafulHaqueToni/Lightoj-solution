87dabbffaec37db2a7ff63c4ce810bfa632c7779
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-04-22 16:58:28 +0600

    Update lightoj 1151.cpp

--- lightoj 1151.cpp
+++ lightoj 1151.cpp
@@ -6,34 +6,32 @@ using namespace std;
 #define eps 1e-6
 #define ll long double
 
-ll a[105][105];
+ll A[105][105];
+ll X[105];
 int To[105];
  
-int Gauss(int n,int m){
-    int col,i,mxr,j,row;
-    for(row=col=1;row<=n&&col<=m;row++,col++){
-        mxr = row;
-        for(i=row+1;i<=n;i++)
-            if(fabs(a[i][col])>fabs(a[mxr][col]))
-                mxr = i;
-        if(mxr != row) swap(a[row],a[mxr]);
-        if(fabs(a[row][col]) < eps){
-            row--;
-            continue;
+void Gauss(int n,int m){
+	 
+    int r,c;
+    for(r=0,c=0;r<n && c<m;c++){
+        for(int i=r+1;i<n;i++) if(fabs(A[i][c])>fabs(A[r][c])) swap(A[i],A[r]);
+        if(fabs(A[r][c])<eps) continue;
+        ll s = A[r][c];
+        for(int i=r+1;i<n;i++) if(fabs(A[i][c])>=eps){
+            ll w = A[i][c];
+            for(int j=c;j<=m;j++){A[i][j]-=A[r][j]/s*w; }
         }
-        for(i=1;i<=n;i++)
-            if(i!=row&&fabs(a[i][col])>eps)
-                for(j=m;j>=col;j--)
-                    a[i][j]-=a[row][j]/a[row][col]*a[i][col];
+        r++;
     }
-    row--;
-    for(int i = row;i>=1;i--){
-        for(int j = i + 1;j <= row;j++){
-			a[i][m] -= a[j][m] * a[i][j];
-        }
-    	a[i][m] /= a[i][i];
+    //Rank = r
+    for(int i=r;i<n;i++) if(fabs(A[i][m])>=eps) return;  //No solution
+    //Unique Solution for r variables
+    for(int i=r-1;i>=0;i--){
+        X[i]=A[i][m];
+        for(int j=i+1;j<r;j++) {X[i]-=(A[i][j]*X[j]); }
+        X[i]/=A[i][i];
+        
     }
-    return row;
 }
 int ii;
  
@@ -41,37 +39,39 @@ void solve()
 {
 	int n;
 	scanf("%d",&n);
-	for(int i=1;i<=100;i++)To[i]=i;
-	for(int i=1;i<=100;i++)
+	for(int i=0;i<=100;i++)To[i]=i;
+	for(int i=0;i<100;i++)
 	{
-		for(int j=1;j<=101;j++)a[i][j]=0;
+		for(int j=0;j<101;j++)A[i][j]=0;
 	}
 	for(int i=1;i<=n;i++)
 	{
 		int x,y;
 		scanf("%d%d",&x,&y);
+		x--;
+		y--;
 		To[x]=y;
 	}
-	a[100][100]=1;
-	for(int i=1;i<=99;i++)
+	A[99][99]=1;
+	for(int i=0;i<99;i++)
 	{
 		if(To[i]==i)
 		{
-			int jabe=min(6,100-i);
-			a[i][i]=jabe;
-			for(int j=1;j<=jabe;j++)a[i][i+j]=-1;
-			a[i][101]=6;
+			int jabe=min(6,99-i);
+			A[i][i]=jabe;
+			for(int j=1;j<=jabe;j++)A[i][i+j]=-1;
+			A[i][100]=6;
 		}
 		else
 		{
-			a[i][i]=1;
-			a[i][To[i]]=-1;
+			A[i][i]=1;
+			A[i][To[i]]=-1;
 		
 		}
 	}
 
-	Gauss(100,101);
-    printf("Case %d: %0.8LF\n",++ii,a[1][101] );
+	Gauss(100,100);
+    printf("Case %d: %0.8LF\n",++ii,X[0] );
 
 }
  
