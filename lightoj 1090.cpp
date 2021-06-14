a5b79d8f5437e93efa1ffec68577e9652d224c26
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-06-14 13:05:35 +0600

    Update lightoj 1090.cpp

--- lightoj 1090.cpp
+++ lightoj 1090.cpp
@@ -1,80 +1,51 @@
+// Created by ash_98
+ 
 #include<bits/stdc++.h>
 using namespace std;
  
+#define mx 200005
 #define ll long long
+#define mod 1000000007
  
-int cnt2,cnt5,n,m,p,q;
- 
- 
-int numberofzeroinfactorial(int n,int div)
+int ar[mx];
+char ch[mx];
+int n,m,ii,k;
+
+ll func(int val,int div)
 {
-    int cnt=0;
- 
-    while(n>0)
+    ll re=0;
+    int pres=div;
+    while(val>=pres)
     {
-        cnt+=n/div;
-        n/=div;
+        re+=val/pres;
+        pres*=div;
     }
-    return cnt;
+    return re;
+}
+ll fact(int val,int p)
+{
+    ll re=0;
+    while(val%p==0)
+    {
+        val/=p;
+        re++;
+    }
+    return re;
 }
  
-int factoraization(int n,int div)
+void solve()
 {
-    int cnt=0;
- 
-    while(n>0&&n%div==0)
-    {
-        cnt++;
-        n/=div;
-    }
- 
-    return cnt;
-}
- 
-void cnt()
-{
-    int a,b,c,d,e,f;
-    a=numberofzeroinfactorial(n,2);
-    b=numberofzeroinfactorial(n,5);
-    c=numberofzeroinfactorial(m,2);
-    c+=numberofzeroinfactorial(n-m,2);
-    d=numberofzeroinfactorial(m,5);
-    d+=numberofzeroinfactorial(n-m,5);
- 
-    e=factoraization(p,2);
-    f=factoraization(p,5);
- 
-    //printf("%d %d %d %d %d %d\n",a,b,c,d,e,f);
- 
-    e*=q;
-    f*=q;
- 
-    cnt2=a-c+e;
-    cnt5=b-d+f;
+    int r,p,q;
+    scanf("%d%d%d%d",&n,&r,&p,&q);
+    ll two=func(n,2)-func(r,2)-func(n-r,2)+fact(p,2)*q;
+    ll five=func(n,5)-func(r,5)-func(n-r,5)+fact(p,5)*q;
+    printf("Case %d: %lld\n",++ii,max(0LL,min(two,five)) );
 }
  
 int main()
 {
-    int t;
+    int t=1;
     scanf("%d",&t);
- 
-    for(int ii=1;ii<=t;ii++)
-    {
-        cnt2=0,cnt5=0;
- 
-        scanf("%d%d%d%d",&n,&m,&p,&q);
- 
-        cnt();
- 
-        printf("Case %d: ",ii);
- 
-        if(cnt2<cnt5)
-        {
-            printf("%d\n",cnt2);
- 
-        }
-        else printf("%d\n",cnt5);
- 
-    }
-    return 9;
+    while(t--)solve();
+    return 0;
 }
