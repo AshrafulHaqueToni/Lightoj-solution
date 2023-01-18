d0b7e566b868d766e88b4c355ec77878ca6cac0e
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2023-01-18 23:27:20 +0600

    Create lightoj 1325.cpp

---
+++ lightoj 1325.cpp
@@ -0,0 +1,62 @@
+// Created by ash_98
+ 
+//Tested : LightOJ 1325 - Distributing Chocolates
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+//Shanks Baby-Step Giant-Step (Faster)
+//a^x = b (mod m)
+ll egcd(ll a,ll b,ll &x,ll &y){
+    if(b == 0) {x = 1; y = 0; return a;}
+    ll g = egcd(b,a%b,y,x);
+    y -= a / b * x;
+    return g;
+}
+
+ll InvMod(ll a,ll m){
+    ll x,y;
+    if(egcd(a,m,x,y) == 1) return (x + m) % m;
+    return -1;
+}
+
+unordered_map<ll, ll> H;
+ll baby_step_giant_step(ll a,ll b,ll m){
+    H.clear();
+    int k = (int) sqrt(m)+1;
+
+    //Baby step
+    ll aa = 1;
+    for(int j=0;j<k;j++){
+        if(!H.count(aa)) H[aa] = j;
+        aa = aa * a % m;
+    }
+
+    //Giant step
+    aa = InvMod(aa,m);
+    for(int i=0;i<k;i++){
+        if(H.count(b)) return i*k + H[b];
+        b = b * aa % m;
+    }
+    return -1;
+}
+
+int ii;
+ 
+void solve()
+{
+    int k,result;
+    scanf("%d%d",&k,&result);
+    //k^x=result
+    printf("Case %d: %lld\n",++ii,baby_step_giant_step(k,result,100000007));
+ 
+}
+ 
+int main()
+{
+    int t=1;
+    scanf("%d",&t);
+    while(t--)solve();
+    return 0;
+}
